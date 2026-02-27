f2ee8d3e27d4564879f0058bf53b6d4771943104
Patsakula Nikita <nikita.patsakula@gmail.com>
2026-02-27 12:51:58 +0300

    Fix: add label_studio to sys.path BEFORE any imports

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ Entry point for PyInstaller executable
 """
 import sys
 import os
-import pathlib
 
 
 def main():
@@ -14,14 +13,12 @@ def main():
         bundle_dir = sys._MEIPASS
         os.environ['LABEL_STUDIO_BASE_DIR'] = bundle_dir
 
-    # Add label_studio directory to sys.path EARLY
-    # This is needed because label_studio modules use relative imports like
-    # "from jwt_auth..." instead of "from label_studio.jwt_auth..."
-    # The _setup_env() in server.py does this too, but it's called too late
-    import label_studio
-    ls_path = str(pathlib.Path(label_studio.__file__).parent.absolute())
-    if ls_path not in sys.path:
-        sys.path.insert(0, ls_path)
+        # Add label_studio directory to sys.path FIRST
+        # This is needed because label_studio modules use imports like
+        # "from jwt_auth..." instead of "from label_studio.jwt_auth..."
+        ls_path = os.path.join(bundle_dir, 'label_studio')
+        if ls_path not in sys.path:
+            sys.path.insert(0, ls_path)
 
     # Import and run label-studio server
     from label_studio.server import main as ls_main
